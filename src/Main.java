a793970fcbfd3d2ee3ebee81f3fd2b890219a33b
lazyHuman <mjbialka@gmail.com>
2025-03-12 19:46:42 +0100

    #1 update the project: added solution for Task2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Task2 task = new Task2();
-        task.main();
+
+        Task2.main();
     }
 }
